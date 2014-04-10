34a6d2c13675505071e3a72e3ad1c08db9bfcd4b
Dan Rosenblatt <dgrosenblatt@gmail.com>
2014-04-09 22:07:55 -0400

    Rebrand Home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,7 +26,7 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading)    { 'Sample App' }
+    let(:heading)    { 'Announciator' }
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
